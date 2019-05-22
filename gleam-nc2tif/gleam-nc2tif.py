3cc1e5a992956dd61889a556d306cbe81547c44b
Koko Alberti <kokoalberti@fastmail.nl>
2019-05-22 17:23:36 +0200

    Add monthly composites option to gleam-nc2tif.py

--- gleam-nc2tif/gleam-nc2tif.py
+++ gleam-nc2tif/gleam-nc2tif.py
@@ -17,14 +17,12 @@ if __name__ == '__main__':
     parser.add_argument('file', metavar='FILE', type=str, 
                                 help='GLEAM NetCDF File')
 
-    parser.add_argument('dir', metavar='OUTPUT_DIRECTORY', type=str, 
-                                help='Output directory')
+    parser.add_argument('target', metavar='TARGET', help='Output filename')
+
+    parser.add_argument('--monthly', help='Output file for monthly average')
+
+
     args = parser.parse_args()
-
-    if not os.path.isdir(args.dir):
-        print("Specified OUTPUT_DIRECTORY is not a directory.")
-        sys.exit(1)
-
     if not os.path.isfile(args.file):
         print("Specified FILE does not exist.")
         sys.exit(1)
@@ -40,7 +38,8 @@ if __name__ == '__main__':
     # Find GLEAM variable and load it
     data = None
     dataset_name = None
-    variable_names = set(('E','Ep','Ei','Eb','Es','Et','Ew','S','SMroot','SMsurf')).intersection(nc.variables.keys())
+    gleam_variables = {'E','Ep','Ei','Eb','Es','Et','Ew','S','SMroot','SMsurf'}
+    variable_names = gleam_variables.intersection(nc.variables.keys())
 
     if len(variable_names) == 0:
         print("No GLEAM variables found in this NetCDF file. Are you sure it" +
@@ -48,7 +47,13 @@ if __name__ == '__main__':
         sys.exit(1)
 
     for variable_name in variable_names:
+        # Obtain the array
         data = nc.variables[variable_name][:,:,:].astype('float32')
+        # Rotate the last 2 axes by 90 degrees
+        data = np.rot90(data, k=1, axes=(1,2))
+        # Flip over the first axis
+        data = np.flip(data, axis=1)
+        # Extract name of the variable
         dataset_name = nc.variables[variable_name].standard_name
     print(f"Using NetCDF variable '{variable_name}' ({dataset_name})")
 
@@ -67,27 +72,17 @@ if __name__ == '__main__':
     srs = osr.SpatialReference()
     srs.ImportFromEPSG(4326)
 
-    # Beware: data.shape is still rotated/mirrored! This is only corrected 
-    # on the fly when writing the band.So still flipping the rows/cols in the 
-    # drv.Create(...) dataset creation.
-    (root, ext) = os.path.splitext(os.path.split(args.file)[-1])
-    target = os.path.join(args.dir, root+'.tif')
-    print(f"Converting {args.file} -> {target}...")
-    ds = drv.Create(target, data.shape[1], data.shape[2], data.shape[0], 
-                            type_code, options=opts)
+    # Create the target dataset
+    print(f"Converting {args.file} -> {args.target}...")
+    ds = drv.Create(args.target, data.shape[2], data.shape[1], data.shape[0], 
+                                 type_code, options=opts)
+
+    if ds is None:
+        print("Could not create output file. Exiting.")
+        sys.exit(1)
 
     ds.SetGeoTransform(geotransform)
     ds.SetProjection(srs.ExportToWkt())
-
-    # Update some metadata so we know how this file came to be.
-    metadata = {
-        'X_CONVERSION_NOTICE': "Converted to GTiff with 'gleam-nc2tif' from " \
-                               "https://github.com/kokoalberti/geocmd/gleam-" \
-                               "nc2tif/",
-        'X_ORIGINAL_FILENAME': os.path.split(args.file)[-1],
-        'X_MORE_INFO': "See http://gleam.eu for info on the GLEAM dataset."
-    }
-    ds.SetMetadata(metadata)
 
     # Write the data into the GTiff bands
     for band_number in range(1, data.shape[0]+1):
@@ -95,10 +90,29 @@ if __name__ == '__main__':
         band.SetNoDataValue(-999)
         band_timeinfo = timestamps[band_number-1].strftime("%B %Y")
         band.SetDescription(f"GLEAM {dataset_name} {band_timeinfo}")
-        band_array = data[band_number-1,::]
-        band_array = np.rot90(band_array)
-        band_array = np.flip(band_array, 0)
-        band.WriteArray(band_array)
+        band.WriteArray(data[band_number-1,::])
 
     ds = None
+
+    # Create a monthly composite if the --monthly option is set
+    if args.monthly:
+        ds = drv.Create(args.monthly, data.shape[2], data.shape[1], 12, 
+                                      type_code, options=opts)
+        if ds is None:
+            print("Could not create output file for monthly composit. Exiting.")
+            sys.exit(1)
+
+        print(f"Creating monthly average composite -> {args.monthly}")
+        ds.SetGeoTransform(geotransform)
+        ds.SetProjection(srs.ExportToWkt())
+        # Create nodata mask
+        for m in range(0, 12):
+            ix = list(range(m, data.shape[0], 12))
+            band = ds.GetRasterBand(m+1)
+            band.SetNoDataValue(-999)
+            band.SetDescription(f"GLEAM {dataset_name} (Monthly Avg Composite)")
+            avg = np.mean(data[ix,:,:], axis=0)
+            band.WriteArray(np.ma.filled(avg,-999))
+        ds = None
+
     print("Done!")
