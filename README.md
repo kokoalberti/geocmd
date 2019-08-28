77776d6a188e4eaeaa3dd8a19d6a7a98120072e1
Koko Alberti <kokoalberti@fastmail.nl>
2019-08-29 00:34:46 +0200

    Add mkprofile script

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Scripts are made for personal use and can probably use some cleaning here and th
 * [mktiles.py](mktiles/mktiles.py) - Make x/y/z map tiles from a GTiff file.
 * [s2c-download.py](s2c-download/s2c-download.py) - Download script for EOX Sentinel 2 Cloudless tiles.
 * [gleam-nc2tif.py](gleam-nc2tif/gleam-nc2tif.py) - Convert NetCDF files outputs from GLEAM model to GeoTiff, and create monthly average composites.
+* [mkprofile.py](mkprofile/mkprofile.py) - Extract profiles from raster data using a warp to a two-point equidistant projection.
---
+++ mkprofile/mkprofile.py
@@ -0,0 +1,71 @@
+"""
+Simple utility program to extract profiles from GDAL data sources using a 
+two-point equidistanct projection.
+
+TODO: - Add option to include coordinates in output csv file. Currently it just
+        records along-profile distance.
+"""
+import argparse
+import sys
+
+from pyproj import CRS, Transformer
+from osgeo import gdal
+
+if __name__ == '__main__':
+    # Parse command line args
+    parser = argparse.ArgumentParser(description="Make profile with GDAL")
+    parser.add_argument('src', metavar='SRC', help='GDAL data source')
+    parser.add_argument('--lon_1', type=float, required=True, help='Longitude of start point')
+    parser.add_argument('--lat_1', type=float, required=True, help='Latitude of start point')
+    parser.add_argument('--lon_2', type=float, required=True, help='Longitude of end point')
+    parser.add_argument('--lat_2', type=float, required=True, help='Latitude of end point')
+    parser.add_argument('--width', type=int, default=100, help='Profile width (m)')
+    parser.add_argument('--dist', type=int, default=100, help='Profile sampling distance (m)')
+    parser.add_argument('--resample', default='near', help='Resampling method')
+    parser.add_argument('--tif', default='', help='Output GTiff file')
+    parser.add_argument('--csv', default='', help='Output CSV file')
+    args = parser.parse_args()
+
+    # Open/validate the source dataset
+    ds = gdal.Open(args.src)
+    if not ds:
+        print("Could not open dataset.")
+        sys.exit(1)
+
+    # Set up coordinate transform
+    proj_str = "+proj=tpeqd +lon_1={} +lat_1={} +lon_2={} +lat_2={}".format(args.lon_1, args.lat_1, args.lon_2, args.lon_2)
+    tpeqd = CRS.from_proj4(proj_str)
+    transformer = Transformer.from_crs(CRS.from_proj4("+proj=latlon"), tpeqd)
+
+    # Transfor to tpeqd coordinates
+    point_1 = transformer.transform(args.lon_1, args.lat_1)
+    point_2 = transformer.transform(args.lon_2, args.lat_2)
+
+    # Create an bounding box (minx, miny, maxx, maxy) in tpeqd coordinates
+    bbox = (point_1[0], -(args.width*0.5), point_2[0], (args.width*0.5))
+
+    # Calculate the number of samples in our profile.
+    num_samples = int((point_2[0] - point_1[0]) / args.dist)
+    print("Reading and warping data into profile swath...")
+
+    # Warp it into dataset in tpeqd projection. If args.tif is empty GDAL will
+    # interpret it as an in-memory dataset.
+    format = 'GTiff' if args.tif else 'VRT'
+    profile = gdal.Warp(args.tif, ds, dstSRS=proj_str, outputBounds=bbox, 
+                        height=1, width=num_samples, resampleAlg=args.resample, format=format)
+
+    # Extract the pixel values and write to an output file
+    data = profile.GetRasterBand(1).ReadAsArray()
+    print("Created {}m profile with {} samples.".format(args.dist*num_samples, num_samples))
+
+    # Write csv output
+    if args.csv:
+        with open(args.csv, 'w') as f:
+            f.write("dist,value\n")
+            for (d, value) in enumerate(data[0,:]):
+                f.write("{},{}\n".format(d*args.dist, value))
+        print("Saved as {}".format(args.csv))
+
+    # Clean up
+    profile = None
+    ds = None
